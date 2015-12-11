756791d6203cb74840e242ea3cf8ef63a4851eb2
alexander.baglay <apofig@gmil.com>
2015-12-11 08:57:51 +0200

    Меню готово

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -4,17 +4,31 @@ package ua.com.juja.sqlcmd.controller;
  * Created by oleksandr.baglai on 11.12.2015.
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.juja.sqlcmd.service.Service;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private Service service;
+
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     public String help() {
         return "help";
+    }
+
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String menu(HttpServletRequest request) {
+        request.setAttribute("items", service.commandsList());
+        return "menu";
     }
 
 }
