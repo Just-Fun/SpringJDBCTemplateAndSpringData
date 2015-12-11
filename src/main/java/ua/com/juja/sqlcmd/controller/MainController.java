025146d9a0f082b63af2ff54c6eb081e50cf90df
alexander.baglay <apofig@gmil.com>
2015-12-11 09:23:08 +0200

    Все перевели на SpringMVC

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -5,14 +5,18 @@ package ua.com.juja.sqlcmd.controller;
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.service.Service;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -20,9 +24,54 @@ public class MainController {
     @Autowired
     private Service service;
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main() {
+        return "redirect:/menu";
+    }
+
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     public String help() {
         return "help";
+    }
+
+    @RequestMapping(value = "/connect", method = RequestMethod.GET)
+    public String connect(HttpSession session) {
+        if (getManager(session) == null) {
+            return "connect";
+        } else {
+            return "menu";
+        }
+    }
+
+    @RequestMapping(value = "/connect", method = RequestMethod.POST)
+    public String connecting(HttpServletRequest request, HttpSession session) {
+        String databaseName = request.getParameter("dbname");
+        String userName = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            DatabaseManager manager = service.connect(databaseName, userName, password);
+            session.setAttribute("db_manager", manager);
+            return "redirect:/menu";
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @RequestMapping(value = "/find", method = RequestMethod.GET)
+    public String find(HttpServletRequest request, HttpSession session) {
+        DatabaseManager manager = getManager(session);
+
+        if (manager == null) { // TODO how to remove duplicate
+            return "redirect:connect";
+        }
+
+        String tableName = request.getParameter("table");
+        request.setAttribute("table", service.find(manager, tableName));
+
+        return "find";
     }
 
     @RequestMapping(value = "/menu", method = RequestMethod.GET)
@@ -31,4 +80,19 @@ public class MainController {
         return "menu";
     }
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(HttpServletRequest request, HttpSession session) {
+        DatabaseManager manager = getManager(session);
+
+        if (manager == null) {
+            return "redirect:connect";
+        }
+
+        request.setAttribute("list", service.tables(manager));
+        return "list";
+    }
+
+    private DatabaseManager getManager(HttpSession session) {
+        return (DatabaseManager) session.getAttribute("db_manager");
+    }
 }
