a177443789eedd97085afe43145a7a8d02676984
alexander.baglay <apofig@gmil.com>
2015-12-19 12:09:50 +0200

    Добавили jdbc template

--- src/main/java/ua/com/juja/sqlcmd/model/JDBCDatabaseManager.java
+++ src/main/java/ua/com/juja/sqlcmd/model/JDBCDatabaseManager.java
@@ -1,5 +1,8 @@
 package ua.com.juja.sqlcmd.model;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -12,41 +15,27 @@ import java.util.*;
 public class JDBCDatabaseManager implements DatabaseManager {
 
     private Connection connection;
+    private JdbcTemplate template;
 
     @Override
     public List<DataSet> getTableData(String tableName) {
-        List<DataSet> result = new LinkedList<DataSet>();
-
-        try (Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM public." + tableName))
-        {
-            ResultSetMetaData rsmd = rs.getMetaData();
-            while (rs.next()) {
-                DataSet dataSet = new DataSetImpl();
-                result.add(dataSet);
-                for (int i = 0; i < rsmd.getColumnCount(); i++) {
-                    dataSet.put(rsmd.getColumnName(i + 1), rs.getObject(i + 1));
+        return template.query("SELECT * FROM public." + tableName,
+                new RowMapper<DataSet>() {
+                    public DataSet mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        ResultSetMetaData rsmd = rs.getMetaData();
+                        DataSet dataSet = new DataSetImpl();
+                        for (int i = 0; i < rsmd.getColumnCount(); i++) {
+                            dataSet.put(rsmd.getColumnName(i + 1), rs.getObject(i + 1));
+                        }
+                        return dataSet;
+                    }
                 }
-            }
-            return result;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return result;
-        }
+            );
     }
 
     @Override
     public int getSize(String tableName) {
-        try (Statement stmt = connection.createStatement();
-             ResultSet rsCount = stmt.executeQuery("SELECT COUNT(*) FROM public." + tableName))
-        {
-            rsCount.next();
-            int size = rsCount.getInt(1);
-            return size;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return 0;
-        }
+        return template.queryForObject("SELECT COUNT(*) FROM public." + tableName, Integer.class);
     }
 
     @Override
@@ -79,6 +68,7 @@ public class JDBCDatabaseManager implements DatabaseManager {
             connection = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/" + database, userName,
                     password);
+            template = new JdbcTemplate(new SingleConnectionDataSource(connection, false));
         } catch (SQLException e) {
             connection = null;
             throw new RuntimeException(
--- src/test/java/ua/com/juja/sqlcmd/model/DatabaseManagerTest.java
+++ src/test/java/ua/com/juja/sqlcmd/model/DatabaseManagerTest.java
@@ -60,6 +60,52 @@ public abstract class DatabaseManagerTest {
     }
 
     @Test
+    public void testGetSize_whenSizeIs0() {
+        // given
+        manager.clear("user");
+
+        // when then
+        assertEquals(0, manager.getSize("user"));
+    }
+
+    @Test
+    public void testGetSize_whenSizeIs1() {
+        // given
+        manager.clear("user");
+
+        DataSet input = new DataSetImpl();
+        input.put("name", "Stiven");
+        input.put("password", "pass");
+        input.put("id", 13);
+        manager.create("user", input);
+
+        // when then
+        assertEquals(1, manager.getSize("user"));
+    }
+
+    @Test
+    public void testGetSize_whenSizeIs2() {
+        // given
+        manager.clear("user");
+
+        DataSet input = new DataSetImpl();
+        input.put("name", "Stiven");
+        input.put("password", "pass");
+        input.put("id", 13);
+        manager.create("user", input);
+
+
+        DataSet input2 = new DataSetImpl();
+        input2.put("name", "Eva");
+        input2.put("password", "***");
+        input2.put("id", 14);
+        manager.create("user", input2);
+
+        // when then
+        assertEquals(2, manager.getSize("user"));
+    }
+
+    @Test
     public void testUpdateTableData() {
         // given
         manager.clear("user");
